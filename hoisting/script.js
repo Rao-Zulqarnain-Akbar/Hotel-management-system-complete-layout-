66c32ef1511a60ecd2421ec5cdf8344a12ecdc7c
Rao-Zulqarnain-Akbar <zulqarnainakbar2606@gmail.com>
2026-08-17 09:52:03 +0500

    Add

---
+++ hoisting/script.js
@@ -0,0 +1,33 @@
+// console.log(name);
+
+// var name = "Ali";
+
+// console.log(name);
+
+   
+// function name(params) {
+//     let a = 1
+//     return ()=>{
+//         a++
+//         console.log(a)
+//     }
+// }
+
+//  let a = name()
+//  let b = name()
+
+//   a()
+//   b()
+
+
+
+function test() {
+
+    var x ;
+
+    console.log(x);
+x = 10;
+}    
+ 
+
+// test()
