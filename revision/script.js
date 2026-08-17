5c717cbcc3c59ff56e71c979dcc46c06a03cd228
Rao-Zulqarnain-Akbar <zulqarnainakbar2606@gmail.com>
2026-08-17 13:08:22 +0500

    Add

---
+++ revision/script.js
@@ -0,0 +1,8 @@
+let menuBtn = document.querySelector("#menuBtn");
+let navLinks = document.querySelector("#navLinks");
+
+menuBtn.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
